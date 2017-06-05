fa76645f21549fdcf0196921a50ba368567f747a
pibicha <pibicha@163.com>
2017-06-05 17:03:47 +0800

    fix

--- readxlxs.py
+++ readxlxs.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
-"""
-解析excel文件，将其中每个sheet页的每行每列，存到运行脚本的目录下
-"""
 import xlrd
 import os
 import sys
-
-dirname = os.path.dirname(sys.argv[0])
-
+reload(sys)
+sys.setdefaultencoding('utf8')
+dirname = os.environ.get(__name__)
 
 def read_excel():
-    workbook = xlrd.open_workbook(sys.argv[1])
-    with open(dirname + 'x.txt', 'w') as f:
-        # 遍历每个sheet
-        for i in range(len(workbook.sheet_names())):
-            sheet = workbook.sheet_by_index(i)
-            rows = sheet.nrows
-            cols = sheet.ncols
-            for j in range(rows):
-                for k in range(cols):
-                    # print sheet.cell(j,k).value
-                    cell = str(sheet.cell(j, k).value)
-                    if cell == '' or cell is None:
-                        f.write('\t')
-                    else:
-                        f.write(cell)
-                    f.write('\t\t\t')
-                # print '\n'
-                f.write('\r\n')
+  # 打开文件
+  workbook = xlrd.open_workbook(sys.argv[1])
 
-
+  with open(result_file,'w') as f:
+  # 遍历每个sheet
+	  for i in range(len(workbook.sheet_names())):
+		sheet = workbook.sheet_by_index(i)
+		rows = sheet.nrows
+		cols = sheet.ncols
+		for j in range(rows):
+			if j in range(start_row):continue
+			for k in range(cols):
+				if k in range(start_col):continue
+				cell = str(sheet.cell(j,k).value)
+				if cell=='' or cell is None:
+					f.write('NULL:')
+					
+				else:
+					f.write(cell + ':')
+				
+			#print '\n'
+			f.write('\r\n')
+ 
 if __name__ == '__main__':
-    read_excel()
+	start_row = input("从第几行开始解析:")
+	start_col = input("从第几列开始解析:")
+	result_file = sys.argv[1].split('.')[0] + '.txt'
+	read_excel()
+	os_path = os.getcwd()
+	print "parse completed ,result save in %s%s" % (os_path,result_file)
